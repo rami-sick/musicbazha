a52a1a4c1a971fb929bf79ba501128d5e438d914
PeDitX <pedram.ale@me.com>
2017-01-12 19:46:45 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,6 +13,10 @@ from sqlalchemy.orm import Session
 from credentials import ENGINE, TOKEN
 from database import Backup
 
+import logging
+import json
+import math
+from aiotg import Bot
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -23,7 +27,11 @@ dp = u.dispatcher
 
 
 def start(bot, update):
-    bot.sendMessage(update.message.chat_id, text="Music Downloader")
+    bot.sendMessage(update.message.chat_id, text="به بات موزیک بازها خوش آمدید ... برای پیدا کردن موزیک مورد نظر اسم خواننده و ترانه مورد نظر را ارسال کنید.."
+                                                 "تمامی حقوق متعلق است به"
+                                                 "PeDitX"
+                                                 "@peditx")
+
 
 
 def music(bot, update):
@@ -50,6 +58,7 @@ def search(text):
     return title, video_url
 
 
+
 def download(title, video_url):
     ydl_opts = {
         'outtmpl': title + '.%(ext)s',
@@ -68,3 +77,50 @@ dp.add_handler(MessageHandler([Filters.text], music))
 
 u.start_polling()
 u.idle()
+
+def search_tracks(chat, query, page=1):
+    logger.info("%s searching for %s", chat.sender, query)
+
+    limit = 8
+    offset = (page - 1) * limit
+
+    cursor = text_search(query).skip(offset).limit(limit)
+    count = await cursor.count()
+    results = await cursor.to_list(limit)
+
+    if count == 0:
+        chat.send_text(not_found)
+        return
+
+    # Return single result if we have exact match for title and performer
+    if results[0]['score'] > 2:
+        limit = 1
+        results = results[:1]
+
+    newoff = offset + limit
+    show_more = count > newoff
+
+    if show_more:
+        pages = math.ceil(count / limit)
+        kb = [['(%d/%d) Show more for "%s"' % (page, pages, query)]]
+        keyboard = {
+            "keyboard": kb,
+            "resize_keyboard": True
+        }
+    else:
+        keyboard = { "hide_keyboard": True }
+
+    for track in results:
+        send_track(chat, keyboard, track)
+
+
+def inline_result(track):
+    return {
+        "type": "audio",
+        "id": track["file_id"],
+        "audio_file_id": track["file_id"],
+        "title": "{} - {}".format(
+            track.get("performer", "Unknown Artist"),
+            track.get("title", "Untitled")
+        )
+    }
